d663d4cd6a2c8eed253c69847bd237e881063c27
BuildTools <unconfigured@null.spigotmc.org>
2022-02-12 11:21:50 +0100

    first

---
+++ src/main/java/io/github/dotheflopboy/basic/Basic.java
@@ -0,0 +1,18 @@
+package io.github.dotheflopboy.basic;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Basic extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        /* Plugin startup logic */
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
